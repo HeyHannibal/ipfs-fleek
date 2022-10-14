9ca78929c3dfa9ebc543b09848bc28607fda7e4f
heyhannibal <hannibal.inq@gmail.com>
2022-10-14 18:26:29 +0300

    thumbs fix

--- gallery/snippt.js
+++ gallery/snippt.js
@@ -1,9 +1,14 @@
 setTimeout(() => {
   let menu = document.querySelector('div[title="Toggle menu (m)"]');
+  // console.log(menu.removeEventListener("click"), "");
+  console.log(getEventListeners(menu));
   menu.childNodes[0].remove();
+
   let a = document.createElement("a");
   a.id = "chldlink";
   a.textContent = "back";
   a.href = "../index.html";
   menu.appendChild(a);
 }, 1);
+let aaa = document.querySelector('div[title="Toggle menu (m)"]');
+console.log(aaa);
